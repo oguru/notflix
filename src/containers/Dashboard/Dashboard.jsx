fd57ba2981466f36eb23872e00042b8dcc8219fd
oguru <oguru@hotmail.co.uk>
2020-06-02 11:01:45 +0100

    pre-jss commit

--- src/containers/Dashboard/Dashboard.jsx
+++ src/containers/Dashboard/Dashboard.jsx
@@ -1,8 +1,29 @@
 import React, { useState, useEffect } from "react";
-import styles from "./Dashboard.module.scss";
+// import styles from "./Dashboard.module.scss";
 import Movies from "../../components/Movies"
 import {Grid, Card} from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import {createUseStyles} from "react-jss";
+
+const useStyles = makeStyles({
+  gridCard: {
+    width: "300px",
+    height: "500px",
+    padding: "10px",
+    border: "black, 10px, solid",
+    "& $cardStyle": {
+      width: "280px",
+      height: "415px",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "space-between"
+    },
+    "& img": {
+      objectFit: "contain"
+    }
+  },
+  
+})
 
 const Dashboard = (props) => {
 
@@ -14,42 +35,33 @@ const Dashboard = (props) => {
     if (movieResults) {
       getMovieDetails(movieResults);
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [movieResults]);
-
+  
   useEffect(() => {
     createMovieCards();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [movieDetails])
+  
+
 
   const getMovieDetails = results => {
     results.forEach(result => fetchData(result.imdbID, "id"));
   };
-
-  const useStyles = makeStyles({
-    cardStyle: {
-      width: "300px",
-      height: "500px",
-      padding: "10px"
-    },
-    cardImg: {
-      width: "280px",
-      height: "415px",
-      objectFit: "cover"
-    }
-  })
 
   const classes = useStyles();
 
   const createMovieCards = () => {      
     const movieMap = movieDetails.map((movie, index) => {
       return (
-        <Grid key={movie.imdbID} onClick={() => showDetails(index)} item xs={12} sm={6} md={4} lg={3} xl={2} >
+        <Grid key={movie.imdbID} onClick={() => showDetails(index)} className={classes.gridCard} item xs={12} sm={6} md={4} lg={3} xl={2} >
           <Card className={classes.cardStyle}>
             <img className={classes.cardImg} src={movie.Poster} alt={movie.Title}/>
-            <h3>{movie.Title}</h3>
-            <p>{movie.Year}</p>
-            <p>{movie.imdbRating}</p>
+            <div>
+              <h3>{movie.Title}</h3>
+              <p>{movie.Year}</p>
+              <p>{movie.imdbRating}</p>
+            </div>
             {/* <p>{movieDetails[imdbId]}</p> */}
           </Card>
         </Grid>
